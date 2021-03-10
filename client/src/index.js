27604cd23851f584da322bd6454de6bf337df567
UnderArbor <enersen1995@gmail.com>
2021-03-09 18:17:20 -0600

    logo flair adjustment

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,9 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import axios from "axios";
 
-if (process.env.NODE_ENV !== 'production') {
-axios.defaults.baseURL = "http://localhost:3000";
+if (process.env.NODE_ENV !== "production") {
+  axios.defaults.baseURL = "http://localhost:3000";
 }
-
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 import "./css/reset.scss";
