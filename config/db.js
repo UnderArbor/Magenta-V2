a6da73ebf5ca62f5eb40eef2a39523bfaec150c9
UnderArbor <enersen1995@gmail.com>
2021-04-18 18:59:40 -0500

    Added env variable for database password

--- config/db.js
+++ config/db.js
@@ -1,20 +1,24 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('MongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("MongoURI");
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGODB_URL || db, {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useUnifiedTopology: true,
-		});
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URL ||
+        `mongodb+srv://UnderArbor:${process.env.DB_PW}@magenta.uluah.mongodb.net/Magenta?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-		console.log('MongoDB Connected...');
-	} catch (err) {
-		console.error(err.message);
-		process.exit(1);
-	}
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
